6f745bc03ca90a59dfea15f05cc4a95185fec142
Anita Aminarti <anitaaminarti6@gmail.com>
2026-05-30 14:39:14 +0800

    feat(database): lengkapi kueri JOIN untuk tampil data reservasi

--- app/src/main/java/com/cozythehotel/database/ReservationDAO.java
+++ app/src/main/java/com/cozythehotel/database/ReservationDAO.java
@@ -21,4 +21,71 @@ public class ReservationDAO {
             galat.printStackTrace();
         }
     }
+
+    
+    public List<Reservation> getReservasiAktif() {
+        List<Reservation> daftar = new ArrayList<>();
+        String kueri = "SELECT r.*, c.name, c.phone, rm.room_number, rm.room_type " +
+                       "FROM reservations r " +
+                       "JOIN customers c ON r.customer_id = c.id " +
+                       "JOIN rooms rm ON r.room_id = rm.id " +
+                       "WHERE r.status = 'Belum Checkout'";
+        try (Connection koneksi = DBConnection.getConnection();
+             Statement pernyataan = koneksi.createStatement();
+             ResultSet hasil = pernyataan.executeQuery(kueri)) {
+            while (hasil.next()) {
+                Reservation res = new Reservation(
+                        hasil.getInt("id"),
+                        hasil.getInt("customer_id"),
+                        hasil.getInt("room_id"),
+                        hasil.getString("check_in"),
+                        hasil.getString("check_out"),
+                        hasil.getString("payment_method"),
+                        hasil.getString("status")
+                );
+                res.setNamaPelanggan(hasil.getString("name"));
+                res.setTeleponPelanggan(hasil.getString("phone"));
+                res.setNomorKamar(hasil.getString("room_number"));
+                res.setTipeKamar(hasil.getString("room_type"));
+                daftar.add(res);
+            }
+        } catch (SQLException galat) {
+            galat.printStackTrace();
+        }
+        return daftar;
+    }
+
+    public List<Reservation> getReservasiBerdasarkanTanggal(String tanggal) {
+        List<Reservation> daftar = new ArrayList<>();
+        String kueri = "SELECT r.*, c.name, c.phone, rm.room_number, rm.room_type " +
+                       "FROM reservations r " +
+                       "JOIN customers c ON r.customer_id = c.id " +
+                       "JOIN rooms rm ON r.room_id = rm.id " +
+                       "WHERE r.check_in <= ? AND r.check_out >= ?";
+        try (Connection koneksi = DBConnection.getConnection();
+             PreparedStatement pernyataan = koneksi.prepareStatement(kueri)) {
+            pernyataan.setString(1, tanggal);
+            pernyataan.setString(2, tanggal);
+            ResultSet hasil = pernyataan.executeQuery();
+            while (hasil.next()) {
+                Reservation res = new Reservation(
+                        hasil.getInt("id"),
+                        hasil.getInt("customer_id"),
+                        hasil.getInt("room_id"),
+                        hasil.getString("check_in"),
+                        hasil.getString("check_out"),
+                        hasil.getString("payment_method"),
+                        hasil.getString("status")
+                );
+                res.setNamaPelanggan(hasil.getString("name"));
+                res.setTeleponPelanggan(hasil.getString("phone"));
+                res.setNomorKamar(hasil.getString("room_number"));
+                res.setTipeKamar(hasil.getString("room_type"));
+                daftar.add(res);
+            }
+        } catch (SQLException galat) {
+            galat.printStackTrace();
+        }
+        return daftar;
+    }
 }
