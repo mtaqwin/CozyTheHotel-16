5800992b893f4d80c3ab11116fd20b11544e418f
mtaqwin <muhtakwiny@gmail.com>
2026-05-30 16:37:49 +0800

    feat(core): integrasi Main class sebagai titik awal aplikasi dan inisialisasi sistem

--- app/src/main/java/com/cozythehotel/Main.java
+++ app/src/main/java/com/cozythehotel/Main.java
@@ -1,16 +1,18 @@
 package com.cozythehotel;
 
+import com.cozythehotel.controller.DashboardController;
+import com.cozythehotel.database.DBConnection;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        // Ini sementara biar aplikasinya bisa jalan dulu tanpa error.
-        // Nanti lu tinggal panggil View atau Dashboard lu di sini.
-        primaryStage.setTitle("CozyThe Hotel - 16");
-        primaryStage.show();
+    public void start(Stage stage) {
+        DBConnection.siapkanBasisData();
+
+        DashboardController dashboard = new DashboardController(stage);
+        dashboard.tampilkan();
     }
 
     public static void main(String[] args) {
